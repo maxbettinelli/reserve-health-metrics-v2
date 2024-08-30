700a41014042b2f4bebee288c1137b8cf7658fef
maxbettinelli <73137949+maxbettinelli@users.noreply.github.com>
2024-08-29 19:27:11 -0700

    get rid of home

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
+
 st.set_page_config(
     page_title="Home",
     page_icon="🏠",
 )
 
 st.sidebar.header("Home")
-st.title("Home")
 
 # Custom CSS for minimal styling
 st.markdown("""
